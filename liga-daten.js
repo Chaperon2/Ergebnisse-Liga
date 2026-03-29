894bc851b647f5a2c4ff69e5c7a2ca660a264d1e
Chaperon2 <chapertomsci@gmail.com>
2026-03-29 15:40:43 +0200

    Update liga-daten.js

--- liga-daten.js
+++ liga-daten.js
@@ -1,1 +1,145 @@
 
+window.STRIKECLUB_DATA = {
+  pageData: {
+    title: "Spieltag 11",
+    date: "25.03.2026",
+    sections: [
+      {
+        type: "table",
+        key: "spieltag",
+        theme: "theme-spieltag",
+        label: "Spieltag 11",
+        raw: `Spieltag 11		
+
+Platz	Name	Team	Spiel 1	Spiel 2	Spiel 3	Spiel 4	Pins Gesamt	⌀
+1	Diana	Die Abräumer	155	206	206	195	762	190,5
+2	Rainsen	Donnerbälle	192	148	204	170	714	178,5
+3	Vegas	Pitchers	189	170	186	165	710	177,5
+4	Rene	Die Abräumer	182	171	167	189	709	177,3
+5	Skampy	PIN FIGHTER	191	163	183	167	704	176,0
+6	Karola	PIN FIGHTER	121	177	153	222	673	168,3
+7	Maik	Die 3 Blinden	181	157	174	125	637	159,3
+8	Thomas	Pitchers	160	157	192	123	632	158,0
+9	Tomber	Pin Bomber	168	167	136	158	629	157,3
+10	Günni	Die 3 Blinden	141	156	148	181	626	156,5
+11	Günter	Gummibande	124	176	144	173	617	154,3
+12	Micha	Pin Bomber	153	147	120	183	603	150,8
+13	Ralf	Donnerbälle	144	154	159	143	600	150,0
+14	Detlef	Stranger Pins	157	148	122	173	600	150,0
+15	Ismene	Donnerbälle	153	138	127	167	585	146,3
+16	Dirk	PIN FIGHTER	129	175	142	130	576	144,0
+17	Benno	Die Abräumer	150	138	118	168	574	143,5
+18	Dieter	Pitchers	127	141	152	100	520	130,0
+19	Ingolf	Pin Bomber	158	140	102	120	520	130,0
+20	Simone	Gummibande	110	131	101	98	440	110,0
+21								
+22								
+23								
+24								
+Bestes Spiel:					Karola	222		
+Hausligaschnitt:					155,4`
+      },
+      {
+        type: "table",
+        key: "einzel",
+        theme: "theme-einzel",
+        label: "Einzelwertung",
+        raw: `Einzelwertung
+Platz	Tendenz	Name	Team	Spiele	Beste Serie	Bestes Spiel	Pins Gesamt	⌀	⌀ letzte Saison	Spiele > 200
+1	0	Skampy	PIN FIGHTER	32	775	224	5692	177,9	171,1	7
+2	0	Rainsen	Donnerbälle	40	774	224	6891	172,3	170,4	5
+3	0	Rene	Die Abräumer	40	758	223	6820	170,5	170,0	5
+4	0	Vegas	Pitchers	40	788	243	6723	168,1	161,4	2
+5	0	Maik	Die 3 Blinden	44	744	239	7299	165,9	169,7	6
+6	0	Tomber	Pin Bomber	39	732	214	6407	164,3	180,3	2
+7	2	Diana	Die Abräumer	44	762	206	7165	162,8	159,6	3
+8	0	Toni	Die 3 Blinden	16	665	207	2562	160,1	156,4	1
+9	2	Karola	PIN FIGHTER	44	703	222	7019	159,5	154,2	2
+10	-3	Micha	Pin Bomber	40	696	199	6379	159,5	0,0	
+11	-1	Thomas	Pitchers	44	677	192	7001	159,1	147,8	
+12	0	Detlef	Stranger Pins	20	663	178	3006	150,3	0,0	
+13	2	Günni	Die 3 Blinden	44	690	199	6583	149,6	145,4	
+14	-1	Ismene	Donnerbälle	40	655	212	5980	149,5	141,8	2
+15	1	Ralf	Donnerbälle	44	673	182	6554	149,0	147,2	
+16	-2	Benno	Die Abräumer	40	717	203	5936	148,4	144,3	1
+17	0	Günter	Gummibande	44	634	195	6483	147,3	153,6	
+18	0	Tom	Stranger Pins	24	723	248	3501	145,9	150,3	1
+19	0	Dirk	PIN FIGHTER	38	660	186	5446	143,3	133,2	
+20	0	Ingolf	Pin Bomber	40	671	184	5503	137,6	142,1	
+21	0	Dieter	Pitchers	40	620	180	5494	137,4	143,8	
+22	0	Simone	Gummibande	36	528	151	4243	117,9	112,7	
+23	0	Denise	Stranger Pins	24	453	142	2606	108,6	0,0	
+24										
+Bestes Spiel:					Tom	248				
+Hausligaschnitt:					152,4`
+      },
+      {
+        type: "composite",
+        key: "team",
+        theme: "theme-team",
+        label: "Teamwertung",
+        top: {
+          label: "Mannschaftswertung",
+          raw: `Platz	Tendenz	Team	Punkte	Pins	Spieltage	⌀
+Platz	Tendenz	Team	Punkte	Pins	Spieltage	⌀
+1	0	Die Abräumer	80	21141	11	160,2
+2	0	PIN FIGHTER	76	20977	11	158,9
+3	1	Pitchers	66	20438	11	154,8
+4	-1	Pin Bomber	66	20294	11	153,7
+5	0	Die 3 Blinden	52	20784	11	157,5
+6	0	Donnerbälle	46	20653	11	156,5
+7	0	Gummibande	31	19106	11	144,7
+8	0	Stranger Pins	23	19400	11	147,0`
+        },
+        bottom: {
+          label: "11. Spieltag · 25.03.2026",
+          raw: `Team	Runde 1	Runde 2	Runde 3	Runde 4	Gesamt	Ergebnis
+	Runde 1	Runde 2	Runde 3	Runde 4	Gesamt	Ergebnis
+PIN FIGHTER	441 + 11	515	478	519 + 10	1974	8
+⚔️	⚔️	⚔️	⚔️	⚔️	⚔️	
+Die 3 Blinden	477	468	477	461	1883	2
+						
+Gummibande	399 + 20	472 + 9	410 + 20	436 + 20	1786	4
+⚔️	⚔️	⚔️	⚔️	⚔️	⚔️	
+Pin Bomber	479	454	358 + 20	461 + 20	1792	6
+						
+Donnerbälle	489	440 + 2	490 + 13	480	1914	2
+⚔️	⚔️	⚔️	⚔️	⚔️	⚔️	
+Die Abräumer	487	517 + 2	511 + 20	552	2067	8
+						
+Pitchers	476 + 13	468	530	388 + 20	1895	8
+⚔️	⚔️	⚔️	⚔️	⚔️	⚔️	
+Stranger Pins	452	443	417	468	1780	2`
+        }
+      }
+    ]
+  },
+
+  rawPlayerTable: String.raw`
+Spieltag 1				Spieltag 2				Spieltag 3				Spieltag 4				Spieltag 5				Spieltag 6				Spieltag 7				Spieltag 8				Spieltag 9				Spieltag 10				Spieltag 11				Spieltag 12				Spieltag 13				Spieltag 14			
+Name	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?	Datum	Pins	Spiele	Anwesend?
+Rainsen	07.01.2026	723	4	true	14.01.2026	707	4	true	21.01.2026	679	4	true	28.01.2026	774	4	true	04.02.2026	617	4	true	11.02.2026	712	4	true	18.02.2026	687	4	true	04.03.2026	0	0	false	11.03.2026	638	4	true	18.03.2026	640	4	true	25.03.2026	714	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Ralf	07.01.2026	673	4	true	14.01.2026	647	4	true	21.01.2026	645	4	true	28.01.2026	500	4	true	04.02.2026	506	4	true	11.02.2026	555	4	true	18.02.2026	611	4	true	04.03.2026	609	4	true	11.03.2026	614	4	true	18.03.2026	594	4	true	25.03.2026	600	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Ismene	07.01.2026	655	4	true	14.01.2026	634	4	true	21.01.2026	577	4	true	28.01.2026	635	4	true	04.02.2026	582	4	true	11.02.2026	575	4	true	18.02.2026	617	4	true	04.03.2026	0	0	false	11.03.2026	616	4	true	18.03.2026	504	4	true	25.03.2026	585	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Maik	07.01.2026	720	4	true	14.01.2026	744	4	true	21.01.2026	692	4	true	28.01.2026	636	4	true	04.02.2026	682	4	true	11.02.2026	590	4	true	18.02.2026	604	4	true	04.03.2026	681	4	true	11.03.2026	670	4	true	18.03.2026	643	4	true	25.03.2026	637	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Toni	07.01.2026	618	4	true	14.01.2026	665	4	true	21.01.2026	629	4	true	28.01.2026	650	4	true	04.02.2026	0	0	false	11.02.2026	0	0	false	18.02.2026	0	0	false	04.03.2026	0	0	false	11.03.2026	0	0	false	18.03.2026	0	0	false	25.03.2026	0	0	false	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Günni	07.01.2026	583	4	true	14.01.2026	569	4	true	21.01.2026	583	4	true	28.01.2026	690	4	true	04.02.2026	525	4	true	11.02.2026	604	4	true	18.02.2026	615	4	true	04.03.2026	557	4	true	11.03.2026	634	4	true	18.03.2026	597	4	true	25.03.2026	626	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Rene	07.01.2026	657	4	true	14.01.2026	758	4	true	21.01.2026	657	4	true	28.01.2026	743	4	true	04.02.2026	647	4	true	11.02.2026	670	4	true	18.02.2026	618	4	true	04.03.2026	0	0	false	11.03.2026	646	4	true	18.03.2026	715	4	true	25.03.2026	709	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Diana	07.01.2026	566	4	true	14.01.2026	687	4	true	21.01.2026	688	4	true	28.01.2026	692	4	true	04.02.2026	578	4	true	11.02.2026	698	4	true	18.02.2026	610	4	true	04.03.2026	655	4	true	11.03.2026	586	4	true	18.03.2026	643	4	true	25.03.2026	762	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Benno	07.01.2026	543	4	true	14.01.2026	628	4	true	21.01.2026	512	4	true	28.01.2026	549	4	true	04.02.2026	0	0	false	11.02.2026	576	4	true	18.02.2026	658	4	true	04.03.2026	618	4	true	11.03.2026	561	4	true	18.03.2026	717	4	true	25.03.2026	574	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Vegas	07.01.2026	683	4	true	14.01.2026	663	4	true	21.01.2026	788	4	true	28.01.2026	0	0	false	04.02.2026	681	4	true	11.02.2026	663	4	true	18.02.2026	592	4	true	04.03.2026	567	4	true	11.03.2026	706	4	true	18.03.2026	670	4	true	25.03.2026	710	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Thomas	07.01.2026	612	4	true	14.01.2026	636	4	true	21.01.2026	662	4	true	28.01.2026	677	4	true	04.02.2026	612	4	true	11.02.2026	666	4	true	18.02.2026	588	4	true	04.03.2026	651	4	true	11.03.2026	609	4	true	18.03.2026	656	4	true	25.03.2026	632	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Dieter	07.01.2026	620	4	true	14.01.2026	0	0	false	21.01.2026	574	4	true	28.01.2026	494	4	true	04.02.2026	521	4	true	11.02.2026	579	4	true	18.02.2026	494	4	true	04.03.2026	616	4	true	11.03.2026	502	4	true	18.03.2026	574	4	true	25.03.2026	520	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Tomber	07.01.2026	703	4	true	14.01.2026	684	4	true	21.01.2026	658	4	true	28.01.2026	592	4	true	04.02.2026	645	4	true	11.02.2026	688	4	true	18.02.2026	438	3	true	04.03.2026	0	0	false	11.03.2026	638	4	true	18.03.2026	732	4	true	25.03.2026	629	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Micha	07.01.2026	601	4	true	14.01.2026	655	4	true	21.01.2026	579	4	true	28.01.2026	0	0	false	04.02.2026	696	4	true	11.02.2026	614	4	true	18.02.2026	667	4	true	04.03.2026	656	4	true	11.03.2026	657	4	true	18.03.2026	651	4	true	25.03.2026	603	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Ingolf	07.01.2026	545	4	true	14.01.2026	671	4	true	21.01.2026	507	4	true	28.01.2026	540	4	true	04.02.2026	569	4	true	11.02.2026	458	4	true	18.02.2026	0	0	false	04.03.2026	635	4	true	11.03.2026	541	4	true	18.03.2026	517	4	true	25.03.2026	520	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Günter	07.01.2026	614	4	true	14.01.2026	570	4	true	21.01.2026	523	4	true	28.01.2026	573	4	true	04.02.2026	567	4	true	11.02.2026	624	4	true	18.02.2026	576	4	true	04.03.2026	634	4	true	11.03.2026	594	4	true	18.03.2026	591	4	true	25.03.2026	617	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Simone	07.01.2026	476	4	true	14.01.2026	462	4	true	21.01.2026	438	4	true	28.01.2026	457	4	true	04.02.2026	512	4	true	11.02.2026	0	0	false	18.02.2026	0	0	false	04.03.2026	454	4	true	11.03.2026	528	4	true	18.03.2026	476	4	true	25.03.2026	440	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Skampy	07.01.2026	0	0	false	14.01.2026	655	4	true	21.01.2026	0	0	false	28.01.2026	731	4	true	04.02.2026	716	4	true	11.02.2026	744	4	true	18.02.2026	0	0	false	04.03.2026	651	4	true	11.03.2026	775	4	true	18.03.2026	716	4	true	25.03.2026	704	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Karola	07.01.2026	635	4	true	14.01.2026	696	4	true	21.01.2026	703	4	true	28.01.2026	571	4	true	04.02.2026	592	4	true	11.02.2026	615	4	true	18.02.2026	658	4	true	04.03.2026	593	4	true	11.03.2026	661	4	true	18.03.2026	622	4	true	25.03.2026	673	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Dirk	07.01.2026	0	0	false	14.01.2026	528	4	true	21.01.2026	552	4	true	28.01.2026	543	4	true	04.02.2026	582	4	true	11.02.2026	567	4	true	18.02.2026	615	4	true	04.03.2026	660	4	true	11.03.2026	594	4	true	18.03.2026	229	2	true	25.03.2026	576	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Detlef	07.01.2026	0	0	false	14.01.2026	645	4	true	21.01.2026	0	0	false	28.01.2026	545	4	true	04.02.2026	0	0	false	11.02.2026	663	4	true	18.02.2026	0	0	false	04.03.2026	0	0	false	11.03.2026	553	4	true	18.03.2026	0	0	false	25.03.2026	600	4	true	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Tom	07.01.2026	541	4	true	14.01.2026	593	4	true	21.01.2026	554	4	true	28.01.2026	723	4	true	04.02.2026	532	4	true	11.02.2026	558	4	true	18.02.2026	0	0	false	04.03.2026	0	0	false	11.03.2026	0	0	false	18.03.2026	0	0	false	25.03.2026	0	0	false	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+Denise	07.01.2026	432	4	true	14.01.2026	426	4	true	21.01.2026	411	4	true	28.01.2026	450	4	true	04.02.2026	434	4	true	11.02.2026	453	4	true	18.02.2026	0	0	false	04.03.2026	0	0	false	11.03.2026	0	0	false	18.03.2026	0	0	false	25.03.2026	0	0	false	01.04.2026	0	0	false	08.04.2026	0	0	false	15.04.2026	0	0	false
+`
+};
